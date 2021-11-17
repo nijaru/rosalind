3be904bd4c698dd03f4ce9f7fa93882b0df85a11
Nick Russo <nijaru0x@gmail.com>
2021-11-16 22:24:02 -0800

    rosalib: add parse_fasta_dict()
    Signed-off-by: Nick Russo <nijaru0x@gmail.com>

--- src/rosalib.py
+++ src/rosalib.py
@@ -2,6 +2,10 @@ import itertools
 
 
 def parse_fasta(fasta):
+    return zip(*parse_fasta_dict(fasta).items())
+
+
+def parse_fasta_dict(fasta):
     # format into lines [1,2...] where 1 is sample id and 2 is sample
     fasta = fasta.strip().split(">")[1:]
     fasta = [x.split("\n", 1) for x in fasta]
@@ -10,4 +14,4 @@ def parse_fasta(fasta):
 
     # create dict {sample_id: sample}
     pairs = {fasta[i]: fasta[i + 1] for i in range(0, len(fasta), 2)}
-    return zip(*pairs.items())
+    return pairs
