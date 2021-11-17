42389aef69ecdb8544bc39df9ca0efada67fac91
Nick Russo <nijaru0x@gmail.com>
2021-11-16 21:47:51 -0800

    lcsm: add working lcsm()
    Signed-off-by: Nick Russo <nijaru0x@gmail.com>

---
+++ src/lcsm.py
@@ -0,0 +1,24 @@
+from rosalib import parse_fasta
+
+FASTA = """>Rosalind_1
+GATTACA
+>Rosalind_2
+TAGACCA
+>Rosalind_3
+ATACA"""
+
+
+def lcsm(fasta):
+    values = list(parse_fasta(fasta).values())
+    sub = ""
+    if len(values) > 1 and len(values[0]) > 0:
+        for i in range(len(values[0])):
+            for j in range(len(values[0]) - i + 1):
+                if j > len(sub) and all(
+                    values[0][i : i + j] in x for x in values
+                ):
+                    sub = values[0][i : i + j]
+    return sub
+
+
+print(lcsm(FASTA))
