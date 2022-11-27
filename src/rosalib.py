1da65aee3e8a34ab024b8ad65cdc3b5df89d8370
Nick Russo <nijaru0x@gmail.com>
2022-11-26 23:46:41 -0800

    rosalib.py: update parse_fasta_dict

--- src/rosalib.py
+++ src/rosalib.py
@@ -1,4 +1,9 @@
-import itertools
+def parse_fasta_dict(fasta):
+    fasta = fasta.strip()
+    fasta = fasta.replace(">", "", 1).split(">")
+    fasta = [x.split("\n", 1) for x in fasta]
+    fasta = {pair[0]: pair[1].replace("\n", "") for pair in fasta}
+    return fasta
 
 
 def parse_fasta(fasta):
@@ -11,34 +16,3 @@ def parse_fasta_keys(fasta):
 
 def parse_fasta_values(fasta):
     return list(parse_fasta_dict(fasta).values())
-
-""">Sample_id
-ACGTACGTACGTACGTACGTACGTACGTACGT
-
->Sample_id
-ACGTACGTACGTACGTACGTACGTACGTACGT
-
->Sample_id
-ACGTACGTACGTACGTACGTACGTACGTACGT
-
->Sample_id
-ACGTACGTACGTACGTACGTACGTACGTACGT
-
->Sample_id
-ACGTACGTACGTACGTACGTACGTACGTACGT
-
->Sample_id
-ACGTACGTACGTACGTACGTACGTACGTACGT
-"""
-
-
-def parse_fasta_dict(fasta):
-    # format into lines [1,2...] where 1 is sample id and 2 is sample
-    fasta = fasta.strip().split(">")[1:]
-    fasta = [x.strip().split("\n") for x in fasta]
-    fasta = list(itertools.chain.from_iterable(fasta))
-    fasta = [x.replace("\n", "") for x in fasta]
-
-    # create dict {sample_id: sample}
-    pairs = {fasta[i]: fasta[i + 1] for i in range(0, len(fasta), 2)}
-    return pairs
